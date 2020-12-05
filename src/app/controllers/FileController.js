f85c2a4aac00b4f1aefd95812d44bfdf99a5e1fc
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2020-12-04 23:19:05 -0300

    Compressao das imagens

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,13 +1,26 @@
+import sharp from 'sharp';
+import { resolve } from 'path';
+import fs from 'fs';
 import File from '../models/File';
 
 class FileController {
   async store(req, res) {
-    const { originalname: name, filename: path } = req.file;
-    const { id, url } = await File.create({ name, path });
+    const { originalname: name, filename, path: filePath } = req.file;
+
+    await sharp(filePath)
+      .resize({ width: 300, withoutEnlargement: true })
+      .webp({ quality: 60 })
+      .toFile(
+        resolve(__dirname, '../', '../', '../', 'tmp', 'uploads', filename)
+      );
+
+    fs.unlinkSync(filePath);
+
+    const { id, url } = await File.create({ name, path: filename });
     return res.json({
       id,
       url,
-      path,
+      path: filename,
     });
   }
 }
--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,14 @@
-import { resolve, extname } from 'path';
+import { resolve } from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    destination: resolve(__dirname, '..', '..', 'tmp', 'tmpUploads'),
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
-        return cb(null, res.toString('hex') + extname(file.originalname));
+        return cb(null, `${res.toString('hex')}.webp`);
       });
     },
   }),
