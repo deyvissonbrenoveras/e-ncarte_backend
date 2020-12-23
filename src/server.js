fe1dc98bcc7574ebcd972a633334993f93bd8b7c
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2020-12-22 22:02:33 -0300

    Teste da nova branch de desenvolvimento

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './app';
 
 const PORT = process.env.APP_PORT;
-
+// TESTE NOVA BRANCH DE DESENVOLVIMENTO
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
